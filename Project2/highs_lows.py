17d9365ffd90af67e900da63ed24ec690ff1a9b2
MostHumblest <69197554+MostHumblest@users.noreply.github.com>
2025-06-11 15:11:08 -0400

    Update highs_lows.py

--- Project2/highs_lows.py
+++ Project2/highs_lows.py
@@ -3,15 +3,15 @@ import csv
 from matplotlib import pyplot as plt
 from datetime import datetime
 
-filename = '.\project2\death_valley_2014.csv'
-with open(filename) as f:
+file_1 = '.\project2\death_valley_2014.csv'
+with open(file_1) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
     for index, column_header in enumerate(header_row):
         print(index, column_header)
 
-    dates, highs, lows = [], [], []
+    dates_1, highs_1, lows_1 = [], [], []
     for row in reader:
         try:
             current_date = datetime.strptime(row[0], "%Y-%m-%d")
@@ -20,17 +20,40 @@ with open(filename) as f:
         except ValueError:
             print(current_date, 'missing data')
         else:
-            dates.append(current_date)
-            highs.append(high)
-            lows.append(low)
+            dates_1.append(current_date)
+            highs_1.append(high)
+            lows_1.append(low)
     
-    print(highs)
+    print("dates in death_valley: " + str(len(dates_1)))
+
+file_2 = '.\project2\sitka_weather_2014.csv'
+with open(file_2) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
+
+    dates_2, highs_2, lows_2 = [], [], []
+    for row in reader:
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])        
+            low = int(row[3])
+        except ValueError:
+            print(current_date, 'missing data')
+        else:
+            dates_2.append(current_date)
+            highs_2.append(high)
+            lows_2.append(low)
+    
+    print("dates in sitka: " + str(len(dates_2)))
 
 #plot data
 fig = plt.figure(dpi=128, figsize=(10,6))
-plt.plot(dates, highs, c='red', alpha=0.5)
-plt.plot(dates, lows, c='blue', alpha=0.5)
-plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+plt.plot(dates_1, highs_1, c='red', alpha=0.5)
+plt.plot(dates_2, highs_2, c='red', alpha=0.5)
+#plt.fill_between(dates_1, highs_1, highs_2, facecolor='blue', alpha=0.1)
 
 plt.title("Daily high and low temperatures - 2014\nDeath Valley, CA", fontsize = 24)
 plt.xlabel('', fontsize=16)
